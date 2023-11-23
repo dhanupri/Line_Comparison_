99fb886f2f5666ddab1892913394e9f7196f3297
unknown <dhanupriasokan@gmail.com>
2023-11-23 19:16:24 +0530

    usecase1

--- Line.java
+++ Line.java
@@ -1,6 +1,6 @@
-import java.utit.*;
+import java.util.Scanner;
 
-class Line {
+public class Line {
     private static point startpoint;
     private static point endpoint;
 
@@ -22,11 +22,50 @@ class Line {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("WELCOME TO LINE COMPARISON COMPUTATION");
-        point startpoint=new point(sc.nextInt(), sc.nextInt());
-        point endpoint=new point(sc.nextInt(),sc.nextInt());
-        Line line=new Line(startpoint,endpoint);
-        double length=Line.calculatelength();
-        System.out.println(length);
+        point startpoint1=new point(sc.nextInt(), sc.nextInt());
+        String s1="";
+        s1+=startpoint1.getX();
+        s1+=startpoint1.getY();
+        point endpoint1=new point(sc.nextInt(),sc.nextInt());
+        String e1="";
+        e1+=endpoint1.getX();
+        e1+=endpoint1.getY();
+        Line line1=new Line(startpoint1,endpoint1);
+        double length_line1=Line.calculatelength();
+        point startpoint2=new point(sc.nextInt(), sc.nextInt());
+        String s2="";
+        s2+=startpoint2.getX();
+        s2+=startpoint2.getY();
+        point endpoint2=new point(sc.nextInt(),sc.nextInt());
+        String e2="";
+        e2+=endpoint2.getX();
+        e2+=endpoint2.getY();
+        Line line2=new Line(startpoint2,endpoint2);
+        double length_line2=Line.calculatelength();
+
+
+
+
+       if((s1.equals(s2))&&(e1.equals(e2)) ){
+         System.out.println("line1 is equal to line2");
+
+       }
+       else if((s1.equals(e2)) && (s2.equals(e1))){
+            System.out.println("line1 is equal to line2");
+
+       }
+       else{
+           System.out.println("line1 is not equal to line2");
+
+       }
+
+
+
+
+
+
+
+        System.out.println(length_line1+" "+length_line2);
 
     }
 
@@ -49,3 +88,4 @@ class point{
         return y;
     }
 }
+
